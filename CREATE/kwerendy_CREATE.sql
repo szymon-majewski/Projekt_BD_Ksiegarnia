08909ead06c4abacc929e9b05f568693f36efc6a
Paulina Purs <79937466+ppurs@users.noreply.github.com>
2022-01-08 23:36:33 +0100

    Update kwerendy_CREATE.sql
    Przeglad: not null, unique + check 'Klienci' + FK w Historii zatrudnien, urlopach, grafiku zmian

--- CREATE/kwerendy_CREATE.sql
+++ CREATE/kwerendy_CREATE.sql
@@ -3,68 +3,75 @@ CREATE TABLE Adresy (
 Miasto NVARCHAR(50) NOT NULL,
 Ulica NVARCHAR(50) NOT NULL,
 [Nr lokalu] INT NOT NULL,
-[Kod pocztowy] NVARCHAR(10)
+[Kod pocztowy] NVARCHAR(10) NOT NULL
 );
 
 CREATE TABLE [Firmy Wysylkowe] (
 [ID Firmy] INT IDENTITY(1, 1) PRIMARY KEY,
-[Nazwa Firmy] NVARCHAR(50) NOT NULL,
+[Nazwa Firmy] NVARCHAR(100) UNIQUE NOT NULL,
 [ID Adresu] INT,
 Telefon INT,
-Email NVARCHAR(50) UNIQUE NOT NULL,
-[Dane przedstawiciela] NVARCHAR(100),
+[E-mail] NVARCHAR(255) UNIQUE NOT NULL,
+[Imie przedstawiciela] NVARCHAR(50) NOT NULL,
+[Nazwisko przedstawiciela] NVARCHAR(50) NOT NULL,
+[Telefon przedstawiciela] INT,
+[E-mail przedstawiciela] NVARCHAR(255) UNIQUE NOT NULL,
+  
 FOREIGN KEY ([ID Adresu]) REFERENCES Adresy([ID Adresu])
 );
 
 
 CREATE TABLE Autorzy (
 [ID Autora] INT IDENTITY(1, 1) PRIMARY KEY,
-Imie NVARCHAR(50),
-Nazwisko NVARCHAR(50)
+Imie NVARCHAR(50) NOT NULL,
+Nazwisko NVARCHAR(50) NOT NULL
 );
 
 
 CREATE TABLE [Urlopy Kategorie] (
 [ID Kategorii] INT IDENTITY(1, 1) PRIMARY KEY,
-Nazwa NVARCHAR(50) NOT NULL,
-[Czy Platny] INT,
-[Dopuszczalna Ilosc Dni] INT,
+Nazwa NVARCHAR(50) UNIQUE NOT NULL,
+[Dopuszczalna Ilosc Dni] INT NOT NULL,
 Opis NVARCHAR(50)
 );
 
 
 CREATE TABLE Rabaty (
 [ID Rabatu] INT IDENTITY(1, 1) PRIMARY KEY,
-Nazwa NVARCHAR(50) NOT NULL,
+Nazwa NVARCHAR(50) UNIQUE NOT NULL,
 [Wysokosc Rabatu] FLOAT NOT NULL
 );
 
 
 CREATE TABLE [Klienci Kategorie] (
 [ID Kategorii] INT IDENTITY(1, 1) PRIMARY KEY,
-Nazwa NVARCHAR(50) NOT NULL,
+Nazwa NVARCHAR(50) UNIQUE NOT NULL,
 [Czy Wymagagane Konto] INT,
-[Dopuszczalna Ilosc Dni] INT,
+[Minimalne Miesieczne Zakupy] MONEY,
+[Maksymalne Miesieczne Zakupy] MONEY,
 Opis NVARCHAR(50),
 [ID Rabatu] INT,
+  
 FOREIGN KEY ([ID Rabatu]) REFERENCES Rabaty([ID Rabatu])
 );
 
 
 CREATE TABLE Serie (
 [ID Serii] INT IDENTITY(1, 1) PRIMARY KEY,
-Nazwa NVARCHAR(50),
+Nazwa NVARCHAR(300) UNIQUE NOT NULL,
 [Ilosc Czesci Planowana] INT,
-[Ilosc Czesci Wydanych] INT
+[Ilosc Czesci Wydanych] INT NOT NULL
 );
 
 
 CREATE TABLE [Punkty Odbioru] (
 [ID Punktu] INT IDENTITY(1, 1) PRIMARY KEY,
-Nazwa NVARCHAR(50) NOT NULL,
-[ID Adresu] INT,
-[Godziny Dzialania] TIME NOT NULL,
+Nazwa NVARCHAR(50) UNIQUE NOT NULL,
+[ID Adresu] INT NOT NULL,
+[Czynny od] TIME NOT NULL,
+[Czynny do] TIME NOT NULL,
 [ID Firmy] INT NOT NULL,
+  
 FOREIGN KEY ([ID Firmy]) REFERENCES [Firmy Wysylkowe]([ID Firmy]),
 FOREIGN KEY ([ID Adresu]) REFERENCES Adresy([ID Adresu])
 );
@@ -75,20 +82,29 @@ CREATE TABLE Klienci (
 [ID Kategorii] INT NOT NULL,
 Imie NVARCHAR(50) NOT NULL,
 Nazwisko NVARCHAR(50) NOT NULL,
-[Nazwa Konta] NVARCHAR(50),
+[Nazwa Konta] NVARCHAR(50) UNIQUE,
 [Haslo Do Konta] NVARCHAR(50),
 [ID Adresu] INT,
 Telefon INT,
-Email NVARCHAR(50) UNIQUE NOT NULL,
+[E-mail] NVARCHAR(255),
+  
 FOREIGN KEY ([ID Kategorii]) REFERENCES [Klienci Kategorie]([ID Kategorii]),
 FOREIGN KEY ([ID Adresu]) REFERENCES Adresy([ID Adresu])
 );
+
+ALTER TABLE Klienci               --nie mozna zalozyc konta bez hasla i e-mailu
+ADD CONSTRAINT CK1_Klienci 
+CHECK ( ([E-mail] IS NOT NULL AND [Nazwa konta] IS NOT NULL AND [Haslo Do Konta] IS NOT NULL ) OR [Nazwa konta] IS NULL )
+
+ALTER TABLE Klienci               --haslo co najmniej 8 znakow
+ADD CONSTRAINT CK2_Klienci CHECK ( [Haslo do konta] >= 8 )
 
 
 CREATE TABLE [Opcje Wysylki] (
 [ID Firmy] INT NOT NULL,
 Typ NVARCHAR(50) NOT NULL,
 Cena MONEY NOT NULL,
+  
 FOREIGN KEY ([ID Firmy]) REFERENCES [Firmy Wysylkowe]([ID Firmy]),
 PRIMARY KEY([ID Firmy], Typ)
 );
@@ -98,7 +114,7 @@ CREATE TABLE Zmiany (
 [ID Zmiany] INT IDENTITY(1, 1) PRIMARY KEY,
 [Godzina Rozpoczecia] TIME NOT NULL,
 [Godzina Zakonczenia] TIME NOT NULL,
-Dzien NVARCHAR(10)
+Dzien NVARCHAR(10) NOT NULL
 );
 
 CREATE TABLE Stanowiska (
@@ -106,7 +122,7 @@ CREATE TABLE Stanowiska (
 Nazwa NVARCHAR(50) NOT NULL UNIQUE,
 Obowiazki NVARCHAR(200),
 Kwalifikacje NVARCHAR(200),
-[Pensja podstawowa] MONEY,
+[Pensja podstawowa] MONEY NOT NULL,
 )
 
 
@@ -123,16 +139,18 @@ FOREIGN KEY ([ID stanowiska]) REFERENCES Stanowiska([ID Stanowiska])
 
 CREATE TABLE Urlopy (
 [ID pracownika] INT,
+[ID kategorii] INT,
 [Data od] DATE,
 [Data do] DATE NOT NULL,
 
 PRIMARY KEY ([ID pracownika], [Data od]),
-FOREIGN KEY ([ID pracownika]) REFERENCES Pracownicy([ID pracownika])
+FOREIGN KEY ([ID pracownika]) REFERENCES Pracownicy([ID pracownika]),
+FOREIGN KEY ([ID kategorii]) REFERENCES [Urlopy Kategorie]([ID Kategorii])
 );
 
 CREATE TABLE [Historia pensji] (
 [ID pracownika] INT,
-[Data zatrudnienia] DATE,
+[Data zatrudnienia] DATE NOT NULL,
 [Data zmiany pensji] DATE NOT NULL,
 
 PRIMARY KEY ([ID pracownika], [Data zatrudnienia]),
@@ -141,11 +159,12 @@ FOREIGN KEY ([ID pracownika]) REFERENCES Pracownicy([ID pracownika])
 
 CREATE TABLE [Historia zatrudnien] (
 [ID pracownika] INT,
-[Data zatrudnienia] DATE,
+[Data zatrudnienia] DATE NOT NULL,
 Stanowisko INT NOT NULL, 
 [Data zwolnienia] DATE,
 
-PRIMARY KEY ([ID pracownika], [Data zatrudnienia])
+PRIMARY KEY ([ID pracownika], [Data zatrudnienia]),
+FOREIGN KEY ([ID pracownika]) REFERENCES Pracownicy([ID pracownika])
 );
 
 
@@ -166,19 +185,19 @@ FOREIGN KEY ([ID punktu odbioru]) REFERENCES [Punkty odbioru]([ID punktu]),
 
 CREATE TABLE [Kategorie ksiazek] (
 [ID kategorii] INT IDENTITY(1,1) PRIMARY KEY,
-Nazwa NVARCHAR(50) NOT NULL,
+Nazwa NVARCHAR(50) UNIQUE NOT NULL,
 );
 
 CREATE TABLE Dostawcy (
 [ID dostawcy] INT IDENTITY(1,1) PRIMARY KEY,
-Nazwa NVARCHAR(50) NOT NULL,
+Nazwa NVARCHAR(50) UNIQUE NOT NULL,
 [ID adresu] INT,
 Telefon INT,
-[E-mail] NVARCHAR(50) NOT NULL,
+[E-mail] NVARCHAR(255) UNIQUE NOT NULL,
 [Imie przedstawiciela] NVARCHAR(50) NOT NULL,
 [Nazwisko przedstawiciela] NVARCHAR(50) NOT NULL,
 [Telefon przedstawiciela] INT,
-[E-mail przedstawiciela] NVARCHAR(50) NOT NULL,
+[E-mail przedstawiciela]  NVARCHAR(255) UNIQUE NOT NULL,
 
 FOREIGN KEY ([ID adresu]) REFERENCES Adresy([ID adresu])
 );
@@ -186,14 +205,14 @@ FOREIGN KEY ([ID adresu]) REFERENCES Adresy([ID adresu])
 
 CREATE TABLE Wydawcy (
 [ID wydawcy] INT IDENTITY(1,1) PRIMARY KEY,
-Nazwa NVARCHAR(50) NOT NULL,
+Nazwa NVARCHAR(50) UNIQUE NOT NULL,
 [ID adresu] INT,
 
 FOREIGN KEY ([ID adresu]) REFERENCES Adresy([ID adresu])
 );
 
 CREATE TABLE Produkty (
-[ID produktu] INT PRIMARY KEY,
+[ID produktu] INT IDENTITY (1,1) PRIMARY KEY,
 [ID kategorii] INT NOT NULL,
 Tytul NVARCHAR(50) NOT NULL,
 [ID autora] INT NOT NULL,
@@ -232,18 +251,21 @@ FOREIGN KEY ([ID produktu]) REFERENCES Produkty([ID produktu])
 
 
 CREATE TABLE [Grafik Zmian] (
-[ID Zmiany] INT NOT NULL,
-[ID Pracownika] INT NOT NULL,
+[ID Zmiany] INT,
+[ID Pracownika] INT,
 [Data Rozpoczecia] DATETIME,
-[Data Zakonczenia] DATETIME
+[Data Zakonczenia] DATETIME NOT NULL,
+  
 FOREIGN KEY ([ID Pracownika]) REFERENCES Pracownicy([ID Pracownika]),
+FOREIGN KEY ([ID Zmiany]) REFERENCES Zmiany([ID Zmiany]),
 PRIMARY KEY ([ID Zmiany], [ID Pracownika], [Data Rozpoczecia])
 );
 
 
 CREATE TABLE [Zapotrzebowanie Na Pracownikow] (
 [ID Stanowiska] INT PRIMARY KEY,
-[Ilosc Potrzebnych Pracownikow] INT,
+[Ilosc Potrzebnych Pracownikow] INT NOT NULL,
+  
 FOREIGN KEY ([ID Stanowiska]) REFERENCES Stanowiska([ID Stanowiska]),
 );
 
@@ -252,8 +274,9 @@ CREATE TABLE [Skargi Pracownicze] (
 [ID Skladajacego] INT,
 [ID Oskarzonego] INT,
 Data DATETIME,
-Tresc NVARCHAR(500),
+Tresc NVARCHAR(500) NOT NULL,
 [Komentarz Przelozonego] NVARCHAR(200),
+  
 FOREIGN KEY ([ID Skladajacego]) REFERENCES Pracownicy([ID Pracownika]),
 FOREIGN KEY ([ID Oskarzonego]) REFERENCES Pracownicy([ID Pracownika]),
 PRIMARY KEY([ID Skladajacego], [ID Oskarzonego], Data)
@@ -261,11 +284,12 @@ PRIMARY KEY([ID Skladajacego], [ID Oskarzonego], Data)
 
 
 CREATE TABLE [Opinie Klientow] (
-[ID Pracownika] INT NOT NULL,
-[ID Klienta] INT NOT NULL,
+[ID Pracownika] INT,
+[ID Klienta] INT,
 Data DATETIME,
 Tresc NVARCHAR(500),
-Ocena INT,
+Ocena INT NOT NULL,
+  
 FOREIGN KEY ([ID pracownika]) REFERENCES Pracownicy([ID pracownika]),
 FOREIGN KEY ([ID Klienta]) REFERENCES Klienci([ID Klienta]),
 PRIMARY KEY ([ID Pracownika], [ID Klienta], Data)
