fac7e24658c39203f7724d149c5dfaa82d2a3fb0
szymon-majewski <szymonmajewski.777@gmail.com>
2022-01-08 20:06:06 +0100

    usuwanie_bazy

---
+++ Usuwanie_bazy_Ksiegarnia.sql
@@ -0,0 +1,92 @@
+IF OBJECT_ID('Urlopy kategorie') IS NOT NULL 
+	DROP TABLE [Urlopy kategorie];
+
+IF OBJECT_ID('Zmiany') IS NOT NULL 
+	DROP TABLE Zmiany;
+
+IF OBJECT_ID('Historia zatrudnien') IS NOT NULL 
+	DROP TABLE [Historia zatrudnien];
+
+IF OBJECT_ID('Historia pensji') IS NOT NULL 
+	DROP TABLE [Historia pensji];
+
+IF OBJECT_ID('Urlopy') IS NOT NULL 
+	DROP TABLE Urlopy;
+
+IF OBJECT_ID('Szczegoly zamowien') IS NOT NULL 
+	DROP TABLE [Szczegoly zamowien];
+
+IF OBJECT_ID('Zamowienia') IS NOT NULL 
+	DROP TABLE Zamowienia;
+
+IF OBJECT_ID('Produkty') IS NOT NULL 
+	DROP TABLE Produkty;
+
+IF OBJECT_ID('Serie') IS NOT NULL 
+	DROP TABLE Serie;
+
+IF OBJECT_ID('Autorzy') IS NOT NULL 
+	DROP TABLE Autorzy;
+
+IF OBJECT_ID('Kategorie ksiazek') IS NOT NULL 
+	DROP TABLE [Kategorie ksiazek];
+
+IF OBJECT_ID('Wydawcy') IS NOT NULL 
+	DROP TABLE Wydawcy;
+
+IF OBJECT_ID('Punkty odbioru') IS NOT NULL 
+	DROP TABLE [Punkty odbioru];
+
+IF OBJECT_ID('Opcje wysylki') IS NOT NULL 
+	DROP TABLE [Opcje wysylki];
+
+IF OBJECT_ID('Firmy wysylkowe') IS NOT NULL 
+	DROP TABLE [Firmy wysylkowe];
+
+IF OBJECT_ID('Dostawcy') IS NOT NULL 
+	DROP TABLE Dostawcy;
+
+IF OBJECT_ID('Przedstawiciele handlowi') IS NOT NULL 
+	DROP TABLE [Przedstawiciele handlowi];
+
+IF OBJECT_ID('Klienci') IS NOT NULL 
+	DROP TABLE Klienci;
+
+IF OBJECT_ID('Klienci Kategorie') IS NOT NULL 
+	DROP TABLE [Klienci Kategorie];
+
+IF OBJECT_ID('Rabaty') IS NOT NULL 
+	DROP TABLE Rabaty;
+
+IF OBJECT_ID('Stanowiska') IS NOT NULL 
+	DROP TABLE Stanowiska;
+
+IF OBJECT_ID('Pracownicy') IS NOT NULL 
+	DROP TABLE Pracownicy;
+
+IF OBJECT_ID('Adresy') IS NOT NULL 
+	DROP TABLE Adresy;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+IF OBJECT_ID('Rabaty') IS NOT NULL 
+	DROP TABLE Rabaty;
+
+IF OBJECT_ID('Rabaty') IS NOT NULL 
+	DROP TABLE Rabaty;
