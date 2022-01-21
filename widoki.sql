7d8bd645577243eab00c10d8a8173f775401ff75
cboats <97353635+cboats@users.noreply.github.com>
2022-01-21 18:58:12 +0100

    Update widoki.sql

--- widoki.sql
+++ widoki.sql
@@ -9,4 +9,15 @@ WHERE Obnizka > 0;
 
 ----------------------------------------------------
 
+IF OBJECT_ID ( 'zamownienia_realizowane') IS NOT NULL
+	DROP VIEW zamownienia_realizowane;
+
+--widok wyświetlający wszystkie zamowienia ktore sa obencie w realizacji
+
+GO
+CREATE VIEW zamownienia_realizowane AS --realizowane to przyjete, oplacone i niewyslane imo
+SELECT * FROM Zamowienia Z
+	LEFT JOIN [Statusy zamowienia] S ON S.[ID Statusu] = Z.[Status wysylki]
+WHERE [Nazwa statusu] = N'oplacono' OR [Nazwa statusu] = N'do wyslania';
+
 
