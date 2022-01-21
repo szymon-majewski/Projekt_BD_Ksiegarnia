787e42464e56fa94897389a63f0c19efa0c9f84a
cboats <97353635+cboats@users.noreply.github.com>
2022-01-21 13:49:08 +0100

    Update wyzwalacze.sql

--- wyzwalacze.sql
+++ wyzwalacze.sql
@@ -11,3 +11,42 @@ AS
 	SET @Text = FORMATMESSAGE('ZAMOWIENIE ZOSTALO ZLOZONE')
 	PRINT @Text
 GO
+
+-- zatrudniono pracownika na danym stanowisku
+IF OBJECT_ID('zatrudniono pracownika', 'TR') IS NOT NULL
+	DROP TRIGGER zatrudniono pracownika
+GO
+
+CREATE TRIGGER zatrudniono pracownika ON Pracownicy
+AFTER INSERT
+AS 
+	DECLARE @Text VARCHAR(256)
+	SET @Text = FORMATMESSAGE('Pracownik zostal dodany do bazy pracownikow')
+	PRINT @Text
+GO
+
+ --wyzwalacz dodanie klienta
+IF OBJECT_ID('dodano klienta', 'TR') IS NOT NULL
+	DROP TRIGGER dodano klienta
+GO
+
+CREATE TRIGGER dodano klienta ON Klienci
+AFTER INSERT
+AS 
+	DECLARE @Text VARCHAR(256)
+	SET @Text = FORMATMESSAGE('konto zostalo zalozone pomyslnie')
+	PRINT @Text
+GO
+
+ --wyzwalacz usuwanie konta
+IF OBJECT_ID('usuniento konto', 'TR') IS NOT NULL
+	DROP TRIGGER usuniento konto
+GO
+
+CREATE TRIGGER usuniento konto ON Klienci
+AFTER DELETE
+AS 
+	DECLARE @Text VARCHAR(256)
+	SET @Text = FORMATMESSAGE('Konto zostalo usuniete pomyslnie')
+	PRINT @Text
+GO
