76f9bbd02092023fd2b8a5071bb7151d6b1d21e4
cboats <97353635+cboats@users.noreply.github.com>
2022-01-21 19:03:45 +0100

    Update widoki.sql

--- widoki.sql
+++ widoki.sql
@@ -20,4 +20,16 @@ SELECT * FROM Zamowienia Z
 	LEFT JOIN [Statusy zamowienia] S ON S.[ID Statusu] = Z.[Status wysylki]
 WHERE [Nazwa statusu] = N'oplacono' OR [Nazwa statusu] = N'do wyslania';
 
+IF OBJECT_ID ( 'pracownicy_obslugii_klienta') IS NOT NULL
+	DROP VIEW pracownicy_obslugii_klienta;
+
+--widok wyświetlający wszytkich pracownikow obslugii klienta
+GO
+CREATE VIEW pracownicy_obslugii_klienta AS
+SELECT * FROM Pracownicy P
+	LEFT JOIN Osoby O ON O.[ID osoby] = P.[ID pracownika]
+WHERE [ID stanowiska] IN 
+(SELECT [ID stanowiska] FROM Stanowiska
+WHERE Nazwa = 'Pracownik Obslugi Klienta');
+
 
