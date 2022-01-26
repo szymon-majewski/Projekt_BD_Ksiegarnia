1e195015068ad67c2e907cfdca72999ee9a6fc5e
cboats <97353635+cboats@users.noreply.github.com>
2022-01-26 15:15:33 +0100

    Update widoki.sql

--- widoki.sql
+++ widoki.sql
@@ -42,4 +42,28 @@ CREATE VIEW zapotrzebowanie_pracownikow AS
 SELECT * FROM [Zapotrzebowanie Na Pracownikow] Z
 WHERE [Ilosc Potrzebnych Pracownikow] > [Ilosc Zatrudnionych Pracownikow];
  
+ 
+ IF OBJECT_ID ( 'hierarchia_pracownikow') IS NOT NULL
+	DROP VIEW hierarchia_pracownikow;
 
+-- widok wyświetlający hierarchie pracownikow
+GO
+CREATE VIEW hierarchia_pracownikow AS
+SELECT O2.Imie AS  [Imie pracowinka], O2.Nazwisko AS  [Nazwisko pracowinka], S2.Nazwa AS [Stanowisko pracowinka], S2.Obowiazki  AS [Obowiazki pracowinka],
+O1.Imie AS [Imie przelozonego], O1.Nazwisko AS  [Nazwisko przelozonego], S1.Nazwa AS [Stanowisko przelozonego],
+P2.[ID pracownika] AS [ID pracownika], P1.[ID pracownika] AS [ID przelozonego]
+FROM Pracownicy AS P1
+ JOIN Pracownicy AS P2 ON P2.[ID przelozonego] = P1.[ID pracownika]
+ JOIN Osoby AS O1 ON O1.[ID osoby] = P1.[ID pracownika]
+ JOIN Osoby AS O2 ON O2.[ID osoby] = P2.[ID pracownika]
+ JOIN Stanowiska AS S2 ON S2.[ID stanowiska] = P2.[ID stanowiska]
+ JOIN Stanowiska AS S1 ON S1.[ID stanowiska] = P1.[ID stanowiska]
+GO
+
+/*
+--testowanie dzialania
+SELECT * FROM hierarchia_pracownikow;
+SELECT * FROM Pracownicy;
+SELECT * FROM Osoby;
+SELECT * FROM Stanowiska;
+*/
