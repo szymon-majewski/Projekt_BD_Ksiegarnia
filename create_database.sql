ea7d8ed0a8a92a03a137e068c394f9c4c937636a
szymon-majewski <szymonmajewski.777@gmail.com>
2022-02-03 16:45:28 +0100

    skrypt usuwajacy baze danych

---
+++ create_database.sql
@@ -0,0 +1,6 @@
+-- Usuniecie bazy danych, jesli istnieje
+USE master
+IF EXISTS( SELECT * FROM sys.databases WHERE name = 'Ksiegarnia Internetowa' )
+DROP DATABASE [Ksiegarnia Internetowa]
+-- Stworzenie bazy danych
+CREATE DATABASE [Ksiegarnia Internetowa];
